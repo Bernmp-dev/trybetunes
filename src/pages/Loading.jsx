6140984614ed3a840719096cb46b93811c656abf
Bernardo Marques <bernardomp.dev@gmail.com>
2022-11-12 19:05:42 -0300

    2nd requirement completed

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export class Loading extends Component {
+  render() {
+    return (
+      <div><h1>Carregando...</h1></div>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,54 @@
 import { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import { Loading } from './Loading';
 
 export class Login extends Component {
+  state = {
+    name: '',
+    isLoading: false,
+    shouldRedirect: false,
+  };
+
+  onChangeHandler = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  };
+
+  onClickHandler = async () => {
+    const { name } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name });
+    this.setState({ isLoading: false, shouldRedirect: true });
+  };
+
   render() {
+    const { name, isLoading, shouldRedirect } = this.state;
+    const min = 3;
+
+    if (shouldRedirect) {
+      this.setState({ shouldRedirect: false });
+      return <Redirect to="/search" />;
+    }
     return (
       <div data-testid="page-login">
-        <p>Login!</p>
+        <input
+          name="name"
+          data-testid="login-name-input"
+          type="text"
+          id="login"
+          onChange={ this.onChangeHandler }
+        />
+        <button
+          data-testid="login-submit-button"
+          type="button"
+          id="login"
+          disabled={ name.length < min }
+          onClick={ this.onClickHandler }
+        >
+          Entrar
+        </button>
+        {isLoading ? <Loading /> : null }
       </div>
     );
   }
