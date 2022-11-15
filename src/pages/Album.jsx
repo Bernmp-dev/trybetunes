cf1719f559b62d5c9b73a8fba79674c83ce790c9
Bernardo Marques <bernardomp.dev@gmail.com>
2022-11-15 15:08:00 -0300

    8th requirement completed

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import getMusics from '../services/musicsAPI';
+import { addSong } from '../services/favoriteSongsAPI';
+import { Loading } from './Loading';
 
 export class Album extends Component {
-  state = { musicIndex: [] };
+  state = { isLoading: false, musicIndex: [], favCheck: [], favSongsId: [] };
 
   async componentDidMount() {
     const { match: { params: { id } } } = this.props;
@@ -11,9 +13,30 @@ export class Album extends Component {
     this.setState({ musicIndex: data });
   }
 
+  // shouldComponentUpdate(nextP, nextS) {
+  //   const { favCheck } = this.state;
+  //   return (favCheck === nextS.favCheck);
+  // }
+
+  checkFavorite = async ({ target }) => {
+    const { name, checked } = target;
+    const { musicIndex, favCheck } = this.state;
+    const selectedSong = musicIndex.find(({ trackName }) => trackName === name);
+    console.log(Array.prototype.indexOf.call(musicIndex, selectedSong));
+
+    this.setState({ favCheck: { [name]: checked, ...favCheck } });
+
+    this.setState({ isLoading: true });
+    await addSong(selectedSong);
+    this.setState({ isLoading: false });
+
+    console.log(favCheck);
+    console.log(name);
+    // console.log(Array.prototype.indexOf.call(favCheck, selectedSong));
+  };
+
   render() {
-    const { musicIndex } = this.state;
-    console.log(musicIndex);
+    const { musicIndex, isLoading } = this.state;
 
     const template = musicIndex
       .map(({ artistName, collectionName, artworkUrl100 }) => (
@@ -24,32 +47,47 @@ export class Album extends Component {
         </div>
       ))[0];
 
-    const musicTemplate = () => (
-      musicIndex.slice(1).map(({ previewUrl, trackName, trackNumber }) => (
-        <div key={ trackName + trackNumber }>
-          <h4>{ trackName }</h4>
-          <audio
-            data-testid="audio-component"
-            src={ previewUrl }
-            controls
-          >
-            <track
-              kind="captions"
-            />
-            O seu navegador não suporta o elemento
-            {' '}
-            <code>audio</code>
-            .
-          </audio>
-        </div>
-      )));
+    const musicTemplate = () => {
+      const { favCheck } = this.state;
 
-    return (
-      <div data-testid="page-album">
-        {template}
-        {musicTemplate()}
-      </div>
-    );
+      return (musicIndex.slice(1)
+        .map(({ previewUrl, trackName, trackNumber, trackId }) => (
+          <div key={ trackName + trackNumber }>
+            <h4>{ trackName }</h4>
+            <audio
+              data-testid="audio-component"
+              src={ previewUrl }
+              controls
+            >
+              <track
+                kind="captions"
+              />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
+            <label htmlFor="Favorita">
+              <input
+                data-testid={ `checkbox-music-${trackId}` }
+                type="checkbox"
+                name={ trackName }
+                id="Favorita"
+                onChange={ (e) => this.checkFavorite(e) }
+                checked={ favCheck[trackName] }
+              />
+            </label>
+          </div>
+        )));
+    };
+
+    return (isLoading
+      ? <Loading /> : (
+        <div data-testid="page-album">
+          {template}
+          {musicTemplate()}
+        </div>
+      ));
   }
 }
 
